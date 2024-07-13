9fe934e460791f12c93287747e714adc73e5760a
안정후 <austinan@naver.com>
2024-07-13 18:16:44 +0900

    feat: core-module init

---
+++ core-module/src/main/java/com/foodgo/coremodule/CoreModuleApplication.java
@@ -0,0 +1,13 @@
+package com.foodgo.coremodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreModuleApplication.class, args);
+    }
+
+}
---
+++ core-module/src/test/java/com/foodgo/coremodule/CoreModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodgo.coremodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
