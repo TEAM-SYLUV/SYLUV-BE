a9a1669bd9cf013c0aa0bf616f0eb851ba7d87b0
안정후 <austinan@naver.com>
2024-07-13 18:01:20 +0900

    chore: project init

---
+++ src/main/java/com/likelion/likelion/LikelionApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likelion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LikelionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/likelion/LikelionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.likelion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikelionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
