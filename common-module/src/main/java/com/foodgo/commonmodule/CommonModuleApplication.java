eaad3a5e0bd8eef1d886c6599e5722357ced20ee
안정후 <austinan@naver.com>
2024-07-13 18:16:34 +0900

    feat: common-module init

---
+++ common-module/src/main/java/com/foodgo/commonmodule/CommonModuleApplication.java
@@ -0,0 +1,13 @@
+package com.foodgo.commonmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonModuleApplication.class, args);
+    }
+
+}
