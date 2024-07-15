bb7aacac3c9df4ce8c4708894a33268e5dd49a98
안정후 <austinan@naver.com>
2024-07-15 19:57:20 +0900

    fix: cors headers & credentials

--- common-module/src/main/java/com/likelion/commonmodule/security/config/CorsConfig.java
+++ common-module/src/main/java/com/likelion/commonmodule/security/config/CorsConfig.java
@@ -2,12 +2,14 @@ package com.likelion.commonmodule.security.config;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CorsConfig implements WebMvcConfigurer {
@@ -22,12 +24,11 @@ public class CorsConfig implements WebMvcConfigurer {
         allowedOriginPatterns.add("https://localhost:3000");
         allowedOriginPatterns.add("https://syluv.link");
 
-        ArrayList<String> allowedHttpMethods = new ArrayList<>();
-        allowedHttpMethods.add("GET");
-        allowedHttpMethods.add("POST");
-
         configuration.setAllowedOrigins(allowedOriginPatterns);
-        configuration.setAllowedMethods(allowedHttpMethods);
+        configuration.setAllowedMethods(List.of("HEAD", "POST", "GET", "DELETE", "PUT", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(List.of(HttpHeaders.LOCATION, HttpHeaders.SET_COOKIE));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
