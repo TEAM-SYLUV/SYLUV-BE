27e6392de2a51784388651cb30d00cc05eb8babd
안정후 <austinan@naver.com>
2024-07-21 23:34:44 +0900

    fix: user token duration

--- api-module/src/main/java/com/likelion/apimodule/user/application/LoginUseCase.java
+++ api-module/src/main/java/com/likelion/apimodule/user/application/LoginUseCase.java
@@ -4,6 +4,7 @@ import com.likelion.apimodule.security.util.JwtUtil;
 import com.likelion.apimodule.user.dto.KakaoLoginRequest;
 import com.likelion.coremodule.user.application.KakaoIdTokenDecodeService;
 import com.likelion.coremodule.user.domain.User;
+import com.likelion.coremodule.user.dto.LoginAddResponse;
 import com.likelion.coremodule.user.dto.LoginResponse;
 import com.likelion.coremodule.user.dto.OidcDecodePayload;
 import com.likelion.coremodule.user.repository.UserRepository;
@@ -21,14 +22,14 @@ public class LoginUseCase {
     private final JwtUtil jwtUtil;
 
     @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public LoginResponse kakaoLogin(final KakaoLoginRequest kakaoLoginRequest) {
+    public LoginAddResponse kakaoLogin(final KakaoLoginRequest kakaoLoginRequest) {
         // ID 토큰으로 찾아온 유저 정보
         final OidcDecodePayload oidcDecodePayload = kakaoIdTokenDecodeService.getPayloadFromIdToken(kakaoLoginRequest.idToken());
 
         final User user = userRepository.findBySubId(oidcDecodePayload.sub())
                 .orElseGet(() -> createNewKakaoUser(oidcDecodePayload));
 
-        return new LoginResponse(jwtUtil.createJwtAccessToken(oidcDecodePayload.email(), oidcDecodePayload.sub()),
+        return new LoginAddResponse(user.getName(), jwtUtil.createJwtAccessToken(oidcDecodePayload.email(), oidcDecodePayload.sub()),
                 jwtUtil.createJwtRefreshToken(oidcDecodePayload.email(), oidcDecodePayload.sub()));
     }
 
