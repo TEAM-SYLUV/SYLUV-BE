c0d0ac69e45574d55ae408d270c72fd00fd1c9e6
안정후 <austinan@naver.com>
2024-07-28 19:17:30 +0900

    fix: for base entity

--- api-module/src/main/java/com/likelion/apimodule/ApiModuleApplication.java
+++ api-module/src/main/java/com/likelion/apimodule/ApiModuleApplication.java
@@ -3,8 +3,10 @@ package com.likelion.apimodule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @ComponentScan(basePackages = "com.likelion")
 public class ApiModuleApplication {
 
