434355134a9b47b7bd9327a7e9a434a9cf448512
안정후 <austinan@naver.com>
2024-08-06 02:03:49 +0900

    fix: marketId 추가

--- api-module/src/main/java/com/likelion/apimodule/store/dto/StoreResponse.java
+++ api-module/src/main/java/com/likelion/apimodule/store/dto/StoreResponse.java
@@ -3,6 +3,7 @@ package com.likelion.apimodule.store.dto;
 import com.likelion.coremodule.store.domain.StoreCategory;
 
 public record StoreResponse(Long storeId,
+                            Long marketId,
                             String name,
                             StoreCategory category,
                             String description,
